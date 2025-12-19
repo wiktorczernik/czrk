7745f194f9540c07895b7023251e9f202730113b
Wiktor Czernik <git@czernik.dev>
2025-12-19 02:23:22 +0100

    Init project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useEffect } from 'react';
+import './App.css';
+
+function App() {
+  useEffect(() => {
+    window.location = "https://discord.gg/bUHMh6VQZv";
+  }, []);
+
+  return <br/>;
+}
+
+export default App
